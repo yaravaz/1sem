9ee7d93a8b0e42912dade152cf5203b912e54c10
yaravaz <115376415+yaravaz@users.noreply.github.com>
2023-03-22 13:25:43 +0300

    Add files via upload

---
+++ ОПИ/1-2лаба/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    system("pause");
+    return 0;
+}
---
+++ ОПИ/1-2лаба/HelloWorld/Lab_02/lab02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	char hello[] = "Hello, ";
+	char fio[] = "Vovna Yaroslava Ryslanovna";
+	std::cout << hello << fio << std::endl;
+	return 0;
+}
